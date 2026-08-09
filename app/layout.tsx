071785c62f5ac8c2e05bb460fb3a9ee08b089b2c
temic137 <68796483+temic137@users.noreply.github.com>
2026-08-09 21:41:48 +0200

    Give Blueprint an industrial CAD aesthetic

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,10 @@ const interfaceFont = localFont({
   variable: "--font-interface",
   display: "swap",
   src: [
-    { path: "./fonts/IBMPlexSans-Regular.ttf", weight: "400" },
-    { path: "./fonts/IBMPlexSans-Medium.ttf", weight: "500" },
-    { path: "./fonts/IBMPlexSans-SemiBold.ttf", weight: "600" },
-    { path: "./fonts/IBMPlexSans-Bold.ttf", weight: "700" },
+    { path: "./fonts/Geist-Regular.ttf", weight: "400" },
+    { path: "./fonts/Geist-Medium.ttf", weight: "500" },
+    { path: "./fonts/Geist-SemiBold.ttf", weight: "600" },
+    { path: "./fonts/Geist-Bold.ttf", weight: "700" },
   ],
 });
 
@@ -17,9 +17,9 @@ const technicalFont = localFont({
   variable: "--font-technical",
   display: "swap",
   src: [
-    { path: "./fonts/IBMPlexMono-Regular.ttf", weight: "400" },
-    { path: "./fonts/IBMPlexMono-Medium.ttf", weight: "500" },
-    { path: "./fonts/IBMPlexMono-SemiBold.ttf", weight: "600" },
+    { path: "./fonts/GeistMono-Regular.ttf", weight: "400" },
+    { path: "./fonts/GeistMono-Medium.ttf", weight: "500" },
+    { path: "./fonts/GeistMono-SemiBold.ttf", weight: "600" },
   ],
 });
 
